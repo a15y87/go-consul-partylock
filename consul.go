14b0ea5fefb09a9c7b13cd9d5a47adb8812d4ef5
Андрей Бардин <a.bardin@dengionline.com>
2016-06-06 12:22:40 +0300

    Initial commit

---
+++ consul.go
@@ -0,0 +1,96 @@
+package go_consul_partylock
+
+import (
+	"github.com/hashicorp/consul/api"
+	"sort"
+	"strings"
+)
+
+type ConsulLockClient struct {
+	TaskPath      string
+	TaskWeight    string
+	ConsulAddress string
+
+	consulSession      string
+	consulClient       *api.Client
+	consulPathWait     string
+	consulPathWaitTask string
+	consulPathLock     string
+	consulPathLockTask string
+}
+
+func (c *ConsulLockClient) Init(TaskPath, TaskWeight, ConsulAddress string) (err error) {
+	se := &api.SessionEntry{}
+	se.TTL = "10s"
+	se.Behavior = "delete"
+
+	ConsulConfig := api.DefaultConfig()
+	ConsulConfig.Address = c.ConsulAddress
+
+	c.consulClient, err = api.NewClient(ConsulConfig)
+	if err != nil {
+		return err
+	}
+
+	c.consulSession, _, err = c.consulClient.Session().Create(se, nil)
+	if err != nil {
+		return err
+	}
+
+	c.consulPathWait = strings.Join([]string{c.TaskPath, "/.wait/"}, "")
+	c.consulPathLock = strings.Join([]string{c.TaskPath, "/.locked/"}, "")
+	c.consulPathWaitTask = strings.Join([]string{c.consulPathWaitTask, c.TaskWeight, "-", c.consulSession}, "")
+	c.consulPathLockTask = strings.Join([]string{c.consulPathLock, c.TaskWeight, "-", c.consulSession}, "")
+
+	return nil
+}
+
+func (c *ConsulLockClient) AddWait() (err error) {
+	kv := c.consulClient.KV()
+	p := &api.KVPair{Key: c.consulPathWaitTask, Value: []byte("0"), Session: c.consulSession}
+	_, _, err = kv.Acquire(p, nil)
+	return err
+}
+
+func (c *ConsulLockClient) DeleteWait() (err error) {
+	kv := c.consulClient.KV()
+	_, err = kv.Delete(c.consulPathWaitTask, nil)
+	return err
+}
+
+func (c *ConsulLockClient) GetWaitPosition() (position int, err error) {
+	kv := c.consulClient.KV()
+	waits, _, err := kv.Keys(c.consulPathWait, ";", nil)
+	if err != nil {
+		return nil, err
+	}
+	position = sort.SearchStrings(waits, c.consulPathWaitTask)
+	return position, err
+}
+
+func (c *ConsulLockClient) AddLock() (status bool, err error) {
+	kv := c.consulClient.KV()
+	p := &api.KVPair{Key: c.consulPathLockTask, Value: []byte("0"), Session: c.consulSession}
+	status, _, err = kv.Acquire(p, nil)
+	return status, err
+}
+
+func (c *ConsulLockClient) DeleteLock() (err error) {
+	kv := c.consulClient.KV()
+	_, err = kv.Delete(c.consulPathLockTask, nil)
+	return err
+}
+
+func (c *ConsulLockClient) GetLocksCount() (count int, err error) {
+	kv := c.consulClient.KV()
+	locks, _, err := kv.Keys(c.consulPathLock, ";", nil)
+	if err != nil {
+		return nil, err
+	}
+	count = len(locks)
+	return count, err
+}
+
+func (c *ConsulLockClient) DestroySession() (err error) {
+	return c.consulClient.Session().Destroy(c.consulSession, nil)
+}
---
+++ partylock.go
@@ -0,0 +1,59 @@
+package go_consul_partylock
+
+import "time"
+
+type PartyLock struct {
+	ConsulClient *ConsulLockClient
+	LockTimeout  int
+	Capacity     int
+}
+
+func New(TaskPath string, TaskWeight string, ConsulAddress string, LockTimeout int) (PartyLock *PartyLock, err error) {
+	PartyLock.LockTimeout = LockTimeout
+	err = PartyLock.ConsulClient.Init(TaskPath, TaskWeight, ConsulAddress)
+	return
+}
+
+func (s *PartyLock) Lock() (status bool, err error) {
+	err = s.ConsulClient.AddWait()
+	if err != nil {
+		return false, err
+	}
+
+	time.Sleep(time.Millisecond * s.LockTimeout)
+	myPosition, err := s.ConsulClient.GetWaitPosition()
+	if err != nil {
+		return false, s.ConsulClient.DeleteWait()
+	}
+	if myPosition > (s.Capacity - 1) {
+
+		return false, s.ConsulClient.DeleteWait()
+	}
+
+	locksCount, err := s.ConsulClient.GetLocksCount()
+	if err != nil {
+		return false, s.ConsulClient.DeleteWait()
+	}
+
+	err = s.ConsulClient.DeleteWait()
+	if err != nil {
+		return false, s.ConsulClient.DeleteWait()
+	}
+
+	if locksCount > (s.Capacity - 1) {
+		return false, err
+	}
+
+	status, err = s.ConsulClient.AddLock()
+	return status, err
+}
+
+
+func (s *PartyLock) UnLock() (status bool, err error) {
+	time.Sleep(time.Millisecond * s.LockTimeout)
+	return s.ConsulClient.DeleteLock()
+}
+
+func (s *PartyLock) Destroy() (err error) {
+	return s.ConsulClient.DestroySession()
+}
